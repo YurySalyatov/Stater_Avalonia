e9856f39eee5e2bda0791fd5264137d183893104
yurys <yury.salyatov@gmail.com>
2025-02-28 19:37:45 +0300

    start project

---
+++ AnalyzeStater/PreparingStater/UnionStater.cs
@@ -0,0 +1,44 @@
+﻿using Stater.Models;
+
+namespace AnalyzeStater.PreparingStater;
+
+public class UnionStater
+{
+    private StateMachine unionStateMachine;
+    
+    private List<StateMachine> stateMachines;
+    
+    public UnionStater(List<StateMachine> stateMachineList)
+    {
+        stateMachines = stateMachineList;
+    }
+
+    public void UnionStateMachines()
+    {
+        // TODO
+        // unionStateMachine = ...
+    }
+
+    public void TranslateToLTLLogic()
+    {
+        // TODO
+        // translate to ltl
+    }
+
+    public void AnalyzeUnionStateMachine()
+    {
+        // TODO
+    }
+
+    public void SimplifyUnionStateMachine()
+    {
+        //TODO
+    }
+
+    public List<StateMachine> UnUnionStateMachines()
+    {
+        // TODO
+        return stateMachines;
+    }
+    
+}
